5beabc5902a1ff16d2c0521f80a265eba3ea9a32
NicholasCramer <nick_cramer@outlook.com>
2025-09-02 10:03:49 -0600

    Added groups to manage objects

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    
     dt = 0
     player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
     
@@ -14,10 +19,13 @@ def main():
             if event.type == pygame.QUIT:
                 return
         
-        player.update(dt)
-          
+        updatable.update(dt)
+        
         screen.fill(color="black")
-        player.draw(screen)
+        
+        for drawables in drawable:
+            drawables.draw(screen)
+          
         pygame.display.flip()
         
         # limit the framerate to 60 FPS
